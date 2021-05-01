e1af243ee41018f36a48ff3c0ba6e0f861b735c0
Devarajathirajan <gokulrajan01234@gmail.com>
2021-05-01 20:55:22 +0530

    refractored entire project structure

--- app.js
+++ app.js
@@ -3,33 +3,33 @@ var mysql = require('mysql');
 var app = express();
 var con;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{
-	res.sendFile(__dirname+'/test.html');
+	res.sendFile(__dirname+'/public/src/index.html');
 });
 app.listen(8000);
 
-con = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-	password:'Deva@01234',
-	database:'onlineshop'
-});
+// con = mysql.createConnection({
+// 	host:'localhost',
+// 	user:'root',
+// 	password:'Deva@01234',
+// 	database:'onlineshop'
+// });
 
-var concallback = (err,res)=>{
-	if(err) throw err;
-	console.log(res);
-};
+// var concallback = (err,res)=>{
+// 	if(err) throw err;
+// 	console.log(res);
+// };
 
-con.connect((err)=>{
-	if(err) throw err;
-	console.log("Connected!!");
+// con.connect((err)=>{
+// 	if(err) throw err;
+// 	console.log("Connected!!");
 
-	//for first run
-	/* 
-	con.query("CREATE DATABASE onlineshop;",concallback);
-	 con.query("USE onlineshop",concallback);
-	 con.query("CREATE TABLE USERLIST(ID INT NOT NULL AUTO_INCREMENT,NAME VARCHAR(30),PASSWORD VARCHAR(13),PRIMARY KEY(ID));",concallback);
-	*/
-	con.query("SELECT * FROM USERLIST;",concallback);
-})
+// 	//for first run
+// 	/* 
+// 	con.query("CREATE DATABASE onlineshop;",concallback);
+// 	 con.query("USE onlineshop",concallback);
+// 	 con.query("CREATE TABLE USERLIST(ID INT NOT NULL AUTO_INCREMENT,NAME VARCHAR(30),PASSWORD VARCHAR(13),PRIMARY KEY(ID));",concallback);
+// 	*/
+// 	con.query("SELECT * FROM USERLIST;",concallback);
+// })
